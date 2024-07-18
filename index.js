04c06d97a2c67ff93bae7fc671a88e406f7fa103
Chetn11 <cnpatil899@gmail.com>
2024-07-19 02:27:46 +0530

    done

--- index.js
+++ index.js
@@ -1,53 +1,46 @@
-const express = require('express');
-const cors=require("cors")
-const bodyParser = require('body-parser');
-const { GoogleGenerativeAI } = require('@google/generative-ai');
-require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
-app.use(cors())
+const port = 5000;
+app.use(cors());
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-app.get("/", async(req,res)=>{
-  return res.send({message:"Api is Working"})
-})
-app.post('/generate', async (req, res) => {
+app.get("/", async (req, res) => {
+  return res.send({ message: "Api is Working" });
+});
+app.post("/generate", async (req, res) => {
   const { prompt } = req.body;
 
   if (!prompt) {
-    return res.status(400).json({ error: 'Prompt is required' });
+    return res.status(400).json({ error: "Prompt is required" });
   }
 
   try {
     // Choose a model that's appropriate for your use case.
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const result = await model.generateContent(`Summarize the following content: ${prompt}`);
-    const response = result.response;
-    const text = response.text();
+    const result = await model.generateContent(
+      `Summarize the following content and also give me sentiment analysis, topic identification and key word extraction of given content in json format: ${prompt}`
+    );
+    const response = result;
+    // const text = response;
 
-    res.json({ text });
+    res.json({ response });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to generate content' });
+    res.status(500).json({ error: "Failed to generate content" });
   }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-
-
-
-
-
-
-
-
 
 // const {GoogleGenerativeAI}=require("@google/generative-ai");
 
